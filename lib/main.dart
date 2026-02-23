b24beb4667bc16318eb3b5e02dc27f91eb871931
1224jatin <jatinsaini54083@gmail.com>
2026-02-23 17:53:06 +0530

    23 feb 2026- image 1

--- lib/main.dart
+++ lib/main.dart
@@ -26,18 +26,91 @@ class MyHome extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text(" First App developed by Jatin", style: TextStyle(
-                fontSize: 20,color: Color(0x0000FFFF)
-              )),
-              const Text("from RBU", style: TextStyle(
-                  fontSize : 25,color: Color(0x000000ff)
+          mainAxisAlignment: MainAxisAlignment.center ,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+              Container( height: 100 ,width: 100 ,color: Colors.brown,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text("POST", style: TextStyle(
+                    color: Colors.black87 , fontSize: 10
+                  ),),
+              const Text("Followers", style: TextStyle(
+                  color: Colors.black87 , fontSize: 10
               ),)
-            ],
-        ),
-      ),
+                ],
+              ),
+            ),
+          Container( height: 100 ,width: 100 ,color: Colors.brown  ,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text("Followers", style: TextStyle(
+                    color: Colors.black87 , fontSize: 10
+                 ),),
+                const Text("1.2k", style: TextStyle(
+                color: Colors.black87 , fontSize: 10
+            ),)
+
+              ],
+
+
+            ),
+          ),
+            Container( height: 100 ,width: 100 ,color: Colors .brown,
+          child:Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text("Followings", style: TextStyle(
+                    color: Colors.black87 , fontSize: 10
+                ),), const Text("180", style: TextStyle(
+                    color: Colors.black87 , fontSize: 10
+                ),)
+              ]
+          )
+
+            )
+]
+            ),
+            Container(
+              height: 200 , width: 300 , color: Colors.blueGrey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment. center,
+                children: [
+                  const Text("About Me ! ", style: TextStyle(
+                    fontSize: 20 , color: Colors.black87
+                  ),), const Text("Passinate Flutter developer with experince  ", style: TextStyle(
+                      fontSize: 10 , color: Colors.black
+                  ),)
+                ],
+              ),
+            ),
+            Container(
+              height: 50 , width: 300 , color: Colors.blue,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text("Follow Me ! ", style: TextStyle( fontSize: 20 , color: Colors.white
+                  ),)
+                ],
+              ),
+            )
+
+             ]
+
+        )
+
+      )
     );
   }
 
